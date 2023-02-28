b2191cd98a15b85fe0a32e68e9567bc43e5f6bce
GSU-EagleAssist <126013953+GSU-EagleAssist@users.noreply.github.com>
2023-02-28 12:21:43 -0500

    Update coastal.py

--- coastal.py
+++ coastal.py
@@ -14,7 +14,7 @@ class coastalSpider(scrapy.Spider):
 		for coastal in response.css('#main'):
 			yield {
 				'What does this team research?' : coastal.css('p:nth-child(7)::text').get() + coastal.css('p:nth-child(8)::text').get(), 
-                'What does this team specialize in?' : coastal.css('h6::text').getall(),# + coastal.css('h6:nth-child(13)::text').get() + coastal.css('h6:nth-child(15)::text').get(),                'What is Conservation Biology and Shoreline Ecology?': coastal.css('p:nth-child(12)::text').get(),
-                'What is Water Quality' : coastal.css('p:nth-child(14)::text').get(),
-                'What are Water Resources' : coastal.css('p:nth-child(16)::text').get()
+                		'What does this team specialize in?' : coastal.css('h6::text').getall(), 
+                		'What is Water Quality' : coastal.css('p:nth-child(14)::text').get(),
+                		'What are Water Resources' : coastal.css('p:nth-child(16)::text').get()
 			}
