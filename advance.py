013e045460d5e320f9f90c155a0e91e7c12d70d0
GSU-EagleAssist <126013953+GSU-EagleAssist@users.noreply.github.com>
2023-02-28 12:20:00 -0500

    Update advance.py

--- advance.py
+++ advance.py
@@ -14,8 +14,8 @@ class advanceSpider(scrapy.Spider):
 		for advance in response.css('#main'):
 			yield {
 				'What does this team research?' : advance.css('p:nth-child(6)::text').get(), 
-                'What does this team specialize in?' : advance.css('p strong::text \n').getall(), 
-                'What is Materials Engineering, Processing, and Characterization?': advance.css('p:nth-child(9)::text').get(),
-                'What is Additive Manufacturing' : advance.css('p:nth-child(11)::text').get(),
-                'What is Plant Operations including Robotics and Automation' : advance.css('p:nth-child(13)::text').get()
+                		'What does this team specialize in?' : advance.css('p strong::text \n').getall(), 
+                		'What is Materials Engineering, Processing, and Characterization?': advance.css('p:nth-child(9)::text').get(),
+                		'What is Additive Manufacturing' : advance.css('p:nth-child(11)::text').get(),
+                		'What is Plant Operations including Robotics and Automation' : advance.css('p:nth-child(13)::text').get()
 			}
