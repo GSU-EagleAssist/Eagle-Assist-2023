c71273bca792e3a8f01046ffd3335f4ac307ec35
GSU-EagleAssist <126013953+GSU-EagleAssist@users.noreply.github.com>
2023-02-28 12:36:01 -0500

    Coastal Sustainability and Resilience
    One of the Impact Areas that the Georgia Southern Research Teams are working on.

--- coastal.py
+++ coastal.py
@@ -14,7 +14,8 @@ class coastalSpider(scrapy.Spider):
 		for coastal in response.css('#main'):
 			yield {
 				'What does this team research?' : coastal.css('p:nth-child(7)::text').get() + coastal.css('p:nth-child(8)::text').get(), 
-                		'What does this team specialize in?' : coastal.css('h6::text').getall(), 
+                		'What does this team specialize in?' : coastal.css('h6::text').getall(),
+                		'What is Conservation Biology and Shoreline Ecology' : coastal.css('p:nth-child(12)::text').get(),
                 		'What is Water Quality' : coastal.css('p:nth-child(14)::text').get(),
                 		'What are Water Resources' : coastal.css('p:nth-child(16)::text').get()
 			}
