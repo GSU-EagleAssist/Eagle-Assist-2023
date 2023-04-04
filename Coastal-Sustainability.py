8fb19714d9842fcfcfcadc3672f54ad25a291c8c
GSU-EagleAssist <126013953+GSU-EagleAssist@users.noreply.github.com>
2023-04-04 12:49:06 -0400

    Update Coastal-Sustainability.py

--- Coastal-Sustainability.py
+++ Coastal-Sustainability.py
@@ -1,7 +1,12 @@
 import scrapy
+from ..items import chatbotEntry
 
 class coastalSpider(scrapy.Spider):
 	name = "coastal"
+
+	custom_settings = {
+        'FEEDS': { 'Coastal.csv': { 'format': 'csv', 'overwrite': 'True', 'item_export_kwargs': {'include_headers_line': False,}}},
+        }
 
 	def start_requests(self):
 		allowed_domains = ['https://research.georgiasouthern.edu']
@@ -12,10 +17,28 @@ class coastalSpider(scrapy.Spider):
 
 	def parse(self, response):
 		for coastal in response.css('#main'):
-			yield {
-				'What does this team research?' : coastal.css('p:nth-child(7)::text').get() + '\n\n' + coastal.css('p:nth-child(8)::text').get(), 
-                'What does this team specialize in?' : coastal.css('h6::text').getall(),
-                'What is Conservation Biology and Shoreline Ecology' : coastal.css('p:nth-child(12)::text').get(),
-                'What is Water Quality' : coastal.css('p:nth-child(14)::text').get(),
-                'What are Water Resources' : coastal.css('p:nth-child(16)::text').get()
-			}
+			
+			q1 = chatbotEntry()
+			q1['Question'] = '"What does the Coastal Sustainability and Resilience team research?"',
+			q1['Answer'] = coastal.css('p:nth-child(7)::text').get() + ' ' + coastal.css('p:nth-child(8)::text').get(), 
+			yield q1
+			
+			q2 = chatbotEntry()
+			q2['Question'] = '"What does Costal Sustainability team specialize in?"',
+			q2['Answer'] = coastal.css('h6::text').getall(),
+			yield q2
+
+			q3 = chatbotEntry()
+			q3['Question'] = '"What is Conservation Biology and Shoreline Ecology?"',
+			q3['Answer'] = coastal.css('p:nth-child(12)::text').get(),
+			yield q3
+
+			q4 = chatbotEntry()
+			q4['Question'] = '"What is Water Quality?"',
+			q4['Answer'] = coastal.css('p:nth-child(14)::text').get(),
+			yield q4
+
+			q5 = chatbotEntry()
+			q5['Question'] = '"What are Water Resources?"', 
+			q5['Answer'] = coastal.css('p:nth-child(16)::text').get()
+			yield q5
