69613d86893f32033a66e151e22b9d534bb53446
GSU-EagleAssist <126013953+GSU-EagleAssist@users.noreply.github.com>
2023-04-04 12:52:42 -0400

    Advanced Materials Research Team

--- Advanced-Materials.py
+++ Advanced-Materials.py
@@ -1,7 +1,12 @@
 import scrapy
+from ..items import chatbotEntry
 
 class advanceSpider(scrapy.Spider):
 	name = "advance"
+
+	custom_settings = {
+        'FEEDS': { 'Advanced-Materials.csv': { 'format': 'csv', 'overwrite': 'True', 'item_export_kwargs': {'include_headers_line': False,}}},
+        }
 
 	def start_requests(self):
 		allowed_domains = ['https://research.georgiasouthern.edu']
@@ -12,10 +17,28 @@ class advanceSpider(scrapy.Spider):
 
 	def parse(self, response):
 		for advance in response.css('#main'):
-			yield {
-				'What does this team research?' : advance.css('p:nth-child(6)::text').get(), 
-                'What does this team specialize in?' : advance.css('p strong::text '+ '\n\n').getall(), 
-                'What is Materials Engineering, Processing, and Characterization?': advance.css('p:nth-child(9)::text').get(),
-                'What is Additive Manufacturing' : advance.css('p:nth-child(11)::text').get(),
-                'What is Plant Operations including Robotics and Automation' : advance.css('p:nth-child(13)::text').get()
-			}
+			
+			q1 = chatbotEntry()
+			q1['Question'] = '"What does the Advanced Materials and Manufacturing team research?"',
+			q1['Answer'] = advance.css('p:nth-child(6)::text').get(),
+			yield q1
+			
+			q2 = chatbotEntry()
+			q2['Question'] = '"What does the Advanced Materials team specialize in?"',
+			q2['Answer'] = advance.css('p strong::text '+ '\n\n').getall(),
+			yield q2
+
+			q3 = chatbotEntry()
+			q3['Question'] = '"What is Materials Engineering, Processing, and Characterization?"',
+			q3['Answer'] = advance.css('p:nth-child(9)::text').get(),
+			yield q3
+
+			q4 = chatbotEntry()
+			q4['Question'] = '"What is Additive Manufacturing?"',
+			q4['Answer'] = advance.css('p:nth-child(11)::text').get(),
+			yield q4
+
+			q5 = chatbotEntry()
+			q5['Question'] = '"What is Plant Operations including Robotics and Automation?"', 
+			q5['Answer'] = advance.css('p:nth-child(13)::text').get()
+			yield q5
