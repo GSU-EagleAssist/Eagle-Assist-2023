c3d8ff85ea93aca81802c1f9ba4004223c10fe75
GSU-EagleAssist <126013953+GSU-EagleAssist@users.noreply.github.com>
2023-04-04 12:51:51 -0400

    Community Enrichment Research Team

--- Community-Enrichment.py
+++ Community-Enrichment.py
@@ -1,7 +1,12 @@
 import scrapy
+from ..items import chatbotEntry
 
 class communitySpider(scrapy.Spider):
 	name = "community"
+
+	custom_settings = {
+        'FEEDS': { 'Community.csv': { 'format': 'csv', 'overwrite': 'True', 'item_export_kwargs': {'include_headers_line': False,}}},
+        }
 
 	def start_requests(self):
 		allowed_domains = ['https://research.georgiasouthern.edu']
@@ -12,10 +17,28 @@ class communitySpider(scrapy.Spider):
 
 	def parse(self, response):
 		for community in response.css('#main'):
-			yield {
-				'What does this team research?' : community.css('p:nth-child(6)::text').get(),
-				'What does this team specialize in?' : community.css('h6:nth-child(8)::text').get() + ',\n' + community.css('h6:nth-child(10)::text').get() + ',\n' + community.css('h6 strong::text').get(),
-				'What is Civic Infrastructures and Public Services?' : community.css('p:nth-child(9)::text').get(),
-				'What is Community Health and Wellbeing?' : community.css('p:nth-child(11)::text').get(),
-				'What do they mean by Quality of Life?' : community.css('p:nth-child(13)::text').get()
-			}
+			
+			q1 = chatbotEntry()
+			q1['Question'] = '"What does the Community Enrichment team research?"',
+			q1['Answer'] = community.css('p:nth-child(6)::text').get(),
+			yield q1
+			
+			q2 = chatbotEntry()
+			q2['Question'] = '"What does the Community Enrichment team specialize in?"',
+			q2['Answer'] = community.css('h6:nth-child(8)::text').get() + ', ' + community.css('h6:nth-child(10)::text').get() + ', ' + community.css('h6 strong::text').get(),
+			yield q2
+
+			q3 = chatbotEntry()
+			q3['Question'] = '"What is Civic Infrastructures and Public Services?"',
+			q3['Answer'] = community.css('p:nth-child(9)::text').get(),
+			yield q3
+
+			q4 = chatbotEntry()
+			q4['Question'] = '"What is Community Health and Wellbeing?"',
+			q4['Answer'] = community.css('p:nth-child(11)::text').get(),
+			yield q4
+
+			q5 = chatbotEntry()
+			q5['Question'] = '"What do they mean by Quality of Life?"', 
+			q5['Answer'] = community.css('p:nth-child(13)::text').get(), 
+			yield q5
